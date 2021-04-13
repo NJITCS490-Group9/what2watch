95aabe80376cc4c09705367d072344c439e8868d
Krupesh <ec2-user@ip-172-31-50-188.ec2.internal>
2021-04-13 21:59:30 +0000

    Add Google API Login/Logout Component

---
+++ src/Logout.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { GoogleLogout } from 'react-google-login';
+
+require('dotenv').config();
+console.log(process.env);
+
+const clientId = process.env.REACT_APP_CLIENTID;
+
+function Logout() {
+  const onSuccess = () => {
+    console.log('Logout made successfully');
+    alert('Logged Out Successfully!');
+  };
+
+  return (
+    <div>
+      <GoogleLogout
+        clientId={clientId}
+        buttonText="Logout"
+        onLogoutSuccess={onSuccess}
+      ></GoogleLogout>
+    </div>
+  );
+}
+
+export default Logout;
