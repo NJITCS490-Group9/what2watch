100f23ab6686839b3bf1d5d483124b3b1febd557
Krupesh <ec2-user@ip-172-31-50-188.ec2.internal>
2021-04-14 22:02:55 +0000

    Add the Host/Join Screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import './App.css';
+import { useState } from 'react';
 import Login from './Login';
 import Logout from './Logout';
+import { MemberHost } from './MemberHost';
 
 function App() {
+  const [isShown, setShown] = useState(true);
+  
+  function onShowHide() {
+    setShown((prevShown) => {
+      return !prevShown;
+    });
+  }
 
   return (
     <div>
-      <h1> Welcome to What2Watch </h1>
-      <Login />
-      <Logout />
+    {isShown === true ? (
+      <div>
+        <h1> Welcome to What2Watch </h1> 
+        <Login /> 
+        <Logout />
+      </div>
+      ) : (
+      ""
+      )}
+      <div>
+        {isShown === true ? (
+          <button class="button" onClick={() => onShowHide()}>Continue{" "}</button>
+        ) : (
+        ""
+        )}
+        {isShown === false ? (
+        <div>
+        <MemberHost />
+        </div>
+        ) : (
+        ""
+        )}
+      </div>
     </div>
   );
 }
