1c72d2002326ea4be3df579df51a44a1d18b14a5
Krupesh <ec2-user@ip-172-31-50-188.ec2.internal>
2021-04-15 21:35:09 +0000

    Make App.js file cleaner

--- src/App.js
+++ src/App.js
@@ -1,53 +1,18 @@
 import './App.css';
 import Login from './Login';
-import Logout from './Logout';
-import React, { useState, useRef, useEffect } from 'react';
-import io from 'socket.io-client';
 import Create from './Create';
-import { MemberHost } from './MemberHost';
 
 
 function App() {
-  const [isShown, setShown] = useState(true);
-  
-  function onShowHide() {
-    setShown((prevShown) => {
-      return !prevShown;
-    });
-  }
+ 
 
   return (
     <div>
     <Navbar>
       <Login />
-      <Logout />
     </Navbar>
-      <h1> Welcome to What2Watch </h1>
       <Create />
-    {isShown === true ? (
-      <div>
-        <h1> Welcome to What2Watch </h1> 
-        <Login /> 
-        <Logout />
       </div>
-      ) : (
-      ""
-      )}
-      <div>
-        {isShown === true ? (
-          <button class="button" onClick={() => onShowHide()}>Continue{" "}</button>
-        ) : (
-        ""
-        )}
-        {isShown === false ? (
-        <div>
-        <MemberHost />
-        </div>
-        ) : (
-        ""
-        )}
-      </div>
-    </div>
   );
 }
 
