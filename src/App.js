f69f7ae8744735e8c9e5ad4a58cd6680f0b54dad
Krupesh <ec2-user@ip-172-31-50-188.ec2.internal>
2021-04-15 22:36:12 +0000

    Fix minor change on App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Login from './Login';
 import Create from './Create';
 import { MemberHost } from './MemberHost';
 import VotingScreen from './VotingScreen';
-import './VotingScreen.css';
+import './VotingScreen.css'; 
 
 
 
