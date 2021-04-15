cd01b917ab422b563fb87a087e6c0ab2ebd2c81d
Krupesh <ec2-user@ip-172-31-50-188.ec2.internal>
2021-04-15 21:52:56 +0000

    Make App.js clear and add Mocked Test Cases

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,8 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { GoogleLogin } from 'react-google-login';
+import Logout from './Logout';
+import { MemberHost } from './MemberHost';
 
 require('dotenv').config();
 console.log(process.env);
@@ -21,9 +23,20 @@ function Login() {
       `Failed to login ${res.profileObj.name}.`
     );
   };
+  
+  const [isShown, setShown] = useState(true);
+  
+  function onShowHide() {
+    setShown((prevShown) => {
+      return !prevShown;
+    });
+  }
 
   return (
     <div>
+    {isShown === true ? (
+      <div>
+      <h1> Welcome to What2Watch </h1>
       <GoogleLogin
         clientId={clientId}
         buttonText="Login"
@@ -33,6 +46,25 @@ function Login() {
         style={{ marginTop: '500px' }}
         isSignedIn={true}
       />
+        <Logout />
+      </div>
+      ) : (
+      ""
+      )}
+      <div>
+        {isShown === true ? (
+          <button class="button" onClick={() => onShowHide()}>Continue{" "}</button>
+        ) : (
+        ""
+        )}
+        {isShown === false ? (
+        <div>
+        <MemberHost />
+        </div>
+        ) : (
+        ""
+        )}
+      </div>
     </div>
   );
 }
