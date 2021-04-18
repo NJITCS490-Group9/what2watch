e842a18a00dff42a99b332cedf1834959ef18164
Ian <ec2-user@ip-172-31-22-22.ec2.internal>
2021-04-18 08:27:49 +0000

    Genres are added into a list to be sent to server. Checking checkbox WIP

--- src/Create.js
+++ src/Create.js
@@ -14,7 +14,7 @@ function Create() {
   const placeRef = useRef(null);
   const host_passcodeRef = useRef(null);
 
-  const [genres, setGenres] = useState([
+  const [genres] = useState([
     { id: 1, value: "Comedy", isChecked: false },
     { id: 2, value: "Action", isChecked: false },
     { id: 3, value: "Horror", isChecked: false },
@@ -28,10 +28,15 @@ function Create() {
     });
   }
   
-  function onChange(event) {
-    const genre = event.target.value;
-    setGenreList(prevList => [...prevList, genre]);
-    console.log(genre)
+  function onHandleBoxClick(e) {
+    const genre = e.target.value;
+    if (!genreList.includes(e.target.value)) {
+      setGenreList(prevList => [...prevList, genre])
+    } else {
+      let index = genreList.indexOf(e.target.value)
+      delete genreList[index]
+    }
+    console.log(genreList)
   }
 
   
@@ -45,18 +50,20 @@ function Create() {
           <p> Date  : <input ref={dateRef} type="date" /> </p>
           <p> Place : <input ref={placeRef} type="text" /> </p>
           <p> Choose genre : </p>
+          <ul>
             { genres.map( genre => (
-              <ul>
+              <li>
                 <input 
                   type="checkbox"
                   key={genre.id}
                   value={genre.value}
                   checked={genre.isChecked}
-                  onClick={onChange, !genre.isChecked}
+                  onClick={(e) => onHandleBoxClick(e)}
                 />
                 {genre.value}
-              </ul>
+              </li>
             )) }
+          </ul>
           <p> Room Passcode <input ref={host_passcodeRef} type="text" /> <button type="submit"> Generate </button> </p> 
           <button type="submit" onClick={() => onCreate()}> Create Room </button>
         </div>
