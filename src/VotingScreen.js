3f6b5db926b0db516c3a390c6d47d1fcd9898bbe
Jessica Bakare <ec2-user@ip-172-31-31-82.ec2.internal>
2021-04-19 13:15:47 +0000

    Minor changes for linting

--- src/VotingScreen.js
+++ src/VotingScreen.js
@@ -1,91 +1,84 @@
 /* eslint-disable */
 import React from 'react';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 import Results from './Results';
-import io from "socket.io-client";
 
 const genreCardData = {
     'Action': 'https://i.imgur.com/aHzf8e9.gif',
     'Comedy': 'https://i.gifer.com/tmJ.gif',
     'Fantasy': 'https://data.whicdn.com/images/298530371/original.gif',
     'Horror': 'https://i.pinimg.com/originals/d8/06/08/d806085699179c5fc12ab69d91830f34.gif',
-    'Romance': 'https://image.freepik.com/free-photo/couple-silhouettes-beach-sunset_106150-110.jpg'
-}
+    'Romance': 'https://image.freepik.com/free-photo/couple-silhouettes-beach-sunset_106150-110.jpg',
+};
 
 const socket = io();
 
-export default function VotingScreen(props)
-{
-    const [genres, setGenres] = useState(["Action", "Comedy", "Fantasy", "Horror", "Romance"]);
-    const [selectedGenre, setSelectedGenre] = useState("");
+export default function VotingScreen() {
+    const [genres, setGenres] = useState(['Action', 'Comedy', 'Fantasy', 'Horror', 'Romance']);
+    const [selectedGenre, setSelectedGenre] = useState('');
     const [hasSelected, toggleSelected] = useState(false);
     let numberOfParticipants = 0;
     const [numVotes, updateNumVotes] = useState(0);
     
-    const voteSelect = (e) =>
-    {
-        console.log("NOTHINGGGGG");
+    const voteSelect = (e) =>{
+        console.log('NOTHINGGGGG');
         console.log(selectedGenre);
         setSelectedGenre(e.target.value);
-        alert("You have chosen " + e.target.value + " Movie.");
-        document.getElementById("submitVote").removeAttribute("disabled");
-        console.log("WOW");
+        alert('You have chosen ' + e.target.value + ' Movie.');
+        document.getElementById('submitVote').removeAttribute('disabled');
+        console.log('WOW');
     }
     
-    function voteSubmit() //to fix: for some reason this function doesn't run when submit button is clicked.
-    {
-        alert("You have submitted your vote! Please wait for the results to be calculated.");
+    function voteSubmit(){//to fix: for some reason this function doesn't run when submit button is clicked.
+        alert('You have submitted your vote! Please wait for the results to be calculated.');
         
         //disable each of the select genre buttons so vote can't be changed after submitting
-        const voteButtons = document.getElementsByClassName("genre_select_btn");
-        for(let i = 0; i < voteButtons.length; i++)
-        {
-            voteButtons[i].setAttribute("disabled", true);
+        const voteButtons = document.getElementsByClassName('genre_select_btn');
+        for(let i = 0; i < voteButtons.length; i++){
+            voteButtons[i].setAttribute('disabled', true);
         }
         
-        switch(selectedGenre)
-        {
-            case "Action":
+        switch(selectedGenre){
+            case 'Action':
                 updateActionVotes(actionVotes + 1);
                 updateNumVotes(numVotes + 1)
                 break;
-            case "Comedy":
+            case 'Comedy':
                 updateComedyVotes(comedyVotes + 1);
                 updateNumVotes(numVotes + 1)
                 break;
-            case "Fantasy":
+            case 'Fantasy':
                 updateFantasyVotes(fantasyVotes + 1);
                 updateNumVotes(numVotes + 1)
                 break;
-            case "Horror":
+            case 'Horror':
                 updateHorrorVotes(horrorVotes + 1);
                 updateNumVotes(numVotes + 1)
                 break;
-            case "Romance":
+            case 'Romance':
                 updateRomanceVotes(romanceVotes + 1);
                 updateNumVotes(numVotes + 1)
                 break;
             default:
-                console.log("Uh oh"); //placeholder for when I can think of a better thing to do for default case
+                console.log('Uh oh'); //placeholder for when I can think of a better thing to do for default case
         }
-        if(numVotes == numberOfParticipants)
-        {
-            socket.emit("vote_complete");
+        if(numVotes == numberOfParticipants){
+            socket.emit('vote_complete');
         }
     }
     
-    let genre_cards = [];
+    const genre_cards = [];
     
-    for (let i = 0; i < genres.length; i++)
-    {
+    for (let i = 0; i < genres.length; i++){
         genre_cards.push(<GenreCard name={genres[i]} voteSelect={voteSelect} />);
     }
     
     useEffect(() =>{
-        socket.on("get_genres", (data) =>{
-            console.log("Genre list received from host.")
-            setGenres(data["genres"]);
-            numberOfParticipants += data["numParticipants"];
+        socket.on('get_genres', (data) =>{
+            console.log('Genre list received from host.')
+            setGenres(data['genres']);
+            numberOfParticipants += data['numParticipants'];
         }, [])
     })
     
@@ -96,10 +89,10 @@ export default function VotingScreen(props)
     }
 
     return (
-      <div className="voting_screen" >
+      <div className='voting_screen' >
         <h2> Movie Genre Vote </h2>
         {genre_cards}
-        <button type="button" className="genre_submit_btn" id= "submitVote" onClick={voteSubmit} disabled> Submit Vote </button>
+        <button type='button' className='genre_submit_btn' id= 'submitVote' onClick={voteSubmit} disabled> Submit Vote </button>
       
       </div>
     );
@@ -108,11 +101,11 @@ export default function VotingScreen(props)
 function GenreCard(props)
 {
     return (
-        <div className="genre_card">
+        <div className='genre_card'>
             <img src={genreCardData[props.name]} alt={props.name}/>
-            <div className="genre_card_container">
+            <div className='genre_card_container'>
                 <h4> {props.name} </h4>
-                <button type="button" className="genre_select_btn" value={props.name} onClick={props.voteSelect}>Select</button>
+                <button type='button' className='genre_select_btn' value={props.name} onClick={props.voteSelect}>Select</button>
             </div>
         </div>
     );    
